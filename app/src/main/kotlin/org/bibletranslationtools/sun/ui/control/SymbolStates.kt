eeae96c6c758e0049455ff0fc9986666b3ad2c7e
Maxim <murmax82@gmail.com>
2024-10-07 15:17:51 +0400

    Fix bug of result icon

--- app/src/main/kotlin/org/bibletranslationtools/sun/ui/control/SymbolStates.kt
+++ app/src/main/kotlin/org/bibletranslationtools/sun/ui/control/SymbolStates.kt
@@ -6,6 +6,10 @@ import android.widget.FrameLayout
 import com.google.android.material.button.MaterialButton
 import org.bibletranslationtools.sun.R
 
+private object Constants {
+    const val NUM_STATES = 4
+}
+
 enum class SymbolState(val value: Int) {
     DEFAULT(0),
     SELECTED(1),
@@ -33,7 +37,12 @@ interface SymbolStateDelegate {
     var state: SymbolState?
 
     fun initState(context: Context, attrs: AttributeSet?) {
-        context.theme.obtainStyledAttributes(attrs, R.styleable.sun, 0, 0).apply {
+        context.theme.obtainStyledAttributes(
+            attrs,
+            R.styleable.sun,
+            0,
+            0
+        ).apply {
             try {
                 state = SymbolState.of(getInt(R.styleable.sun_state_symbol, 0))
             } finally {
@@ -66,7 +75,7 @@ class SymbolFrameLayout @JvmOverloads constructor(
     }
 
     override fun onCreateDrawableState(extraSpace: Int): IntArray {
-        val baseState = super.onCreateDrawableState(extraSpace + 4)
+        val baseState = super.onCreateDrawableState(extraSpace + Constants.NUM_STATES)
         return mergeDrawableStates(baseState, getSymbolDrawableState())
     }
 }
@@ -77,13 +86,41 @@ class SymbolButton @JvmOverloads constructor(
     defStyleAttr: Int = 0
 ) : MaterialButton(context, attrs, defStyleAttr), SymbolStateDelegate {
     override var state: SymbolState? = SymbolState.DEFAULT
+        set(value) {
+            field = value
+            updateIcon()
+            refreshDrawableState()
+        }
 
     init {
         initState(context, attrs)
     }
 
     override fun onCreateDrawableState(extraSpace: Int): IntArray {
-        val baseState = super.onCreateDrawableState(extraSpace + 4)
+        val baseState = super.onCreateDrawableState(extraSpace + Constants.NUM_STATES)
         return mergeDrawableStates(baseState, getSymbolDrawableState())
+    }
+
+    override fun refreshDrawableState() {
+        super.refreshDrawableState()
+        if (icon != null) {
+            val currentState = getSymbolDrawableState()
+            val changed = icon.setState(currentState)
+
+            // Force the view to draw if icon state has changed.
+            if (changed) {
+                invalidate()
+            }
+        }
+    }
+
+    private fun updateIcon() {
+        // We should manually update button icon on state change
+        // because MaterialButton doesn't do that automatically.
+        when (state) {
+            SymbolState.CORRECT -> setIconResource(R.drawable.ic_check_24)
+            SymbolState.INCORRECT -> setIconResource(R.drawable.ic_close_24)
+            else -> setIconResource(R.color.transparent)
+        }
     }
 }
